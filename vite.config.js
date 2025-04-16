7b90182d884ac675debe6850ebd210185eb04fd5
kundan singh patel <kundansingh066@gmail.com>
2025-04-16 14:14:07 +0530

    render deployement issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 10000, // optional, Render provides $PORT anyway
+    host: true, // binds to 0.0.0.0
+    allowedHosts: ["https://calendlly-client.onrender.com/"],
+  },
+});
